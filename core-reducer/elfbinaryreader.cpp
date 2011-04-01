deceb1f1493dfa64bb8189d59ce8400e6d8e2bc3
Sampo Saaristo <sampo.saaristo@sofica.fi>
2011-04-01 10:25:07 +0300

    invalid return value handling for elf_getphdrnum() and elf_getshdrstrndx() fixed

--- core-reducer/elfbinaryreader.cpp
+++ core-reducer/elfbinaryreader.cpp
@@ -88,10 +88,10 @@ bool ElfBinaryReader::initalize(const char *fileName)
         LOG_RETURN(LOG_ERR, false, "Can not read the elf program headers for '%s'.", elf_errmsg(-1));
 #endif
 
-    if (elf_getphdrnum(file, &programHeaderNumber) == 0)
+    if (elf_getphdrnum(file, &programHeaderNumber) == -1)
         LOG_RETURN(LOG_ERR, false, "getphdrnum() failed: %s", elf_errmsg(-1));
 
-    if(elf_getshdrstrndx(file, &sectionHeaderStringIndex) == 0)
+    if(elf_getshdrstrndx(file, &sectionHeaderStringIndex) == -1)
         LOG_RETURN(LOG_ERR, false, "elf_getshdrstrndx() failed: %s", elf_errmsg(-1));
 
     return true;
